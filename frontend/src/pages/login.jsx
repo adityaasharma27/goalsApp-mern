c014a1778eac53a861112db4ad6f85998f89e2e4
Aditya Sharma <adityaasharma27@gmail.com>
2022-03-16 09:58:38 +0530

    Frontend setup

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,8 +1,68 @@
+import {useState, useEffect} from 'react'
+import {FaSignInAlt} from 'react-icons/fa'
 
-function login() {
-  return (
-    <div>login</div>
-  )
+function Login() {
+  const [formData, setFormData] = useState({
+    
+    email:'',
+    password:''
+  })
+
+  const {email, password} = formData
+
+  const onChange = (e)=>{
+    setFormData((prevState)=>({
+      ...prevState,
+      [e.target.name]:e.target.value,
+    }))
+  }
+
+  const onSubmit = (e)=>{
+    e.preventDefault();
+    
+  }
+
+  return <>
+    <section classname="heading">
+      <h1>
+        <FaSignInAlt /> Login
+      </h1>
+      <p> Login and start setting Goals</p>
+    </section>
+
+    <section classname="form">
+    <form onSubmit='onSubmit'>
+      
+      <div classname='form-group'>
+        <input 
+        type='email' 
+        className='form-control'
+        id='email'
+        name='email'
+        value={email}
+        placeholder='Enter your email'
+        onChange={onChange}
+        />
+      </div>
+      <div classname='form-group'>
+        <input 
+        type='password' 
+        className='form-control'
+        id='password'
+        name='password'
+        value={password}
+        placeholder='Enter password'
+        onChange={onChange}
+        />
+      </div>
+      
+      <div classname='form-group'>
+        <button type='submit' classname='btn btn-block'>Submit</button>
+      </div>
+    </form>
+    </section>
+  </>
+  
 }
 
-export default login
+export default Login
--- frontend/src/pages/register.jsx
+++ frontend/src/pages/register.jsx
@@ -1,9 +1,89 @@
+import {useState, useEffect} from 'react'
+import {FaUser} from 'react-icons/fa'
 
+function Register() {
+  const [formData, setFormData] = useState({
+    name:'',
+    email:'',
+    password:'',
+    password2:''
+  })
 
-function register() {
-  return (
-    <div>register</div>
-  )
+  const {name, email, password, password2} = formData
+
+  const onChange = (e)=>{
+    setFormData((prevState)=>({
+      ...prevState,
+      [e.target.name]:e.target.value,
+    }))
+  }
+
+  const onSubmit = (e)=>{
+    e.preventDefault();
+    
+  }
+
+  return <>
+    <section classname="heading">
+      <h1>
+        <FaUser /> Register
+      </h1>
+      <p> Please create an account</p>
+    </section>
+
+    <section classname="form">
+    <form onSubmit='onSubmit'>
+      <div classname='form-group'>
+        <input 
+        type='text' 
+        className='form-control'
+        id='name'
+        name='name'
+        value={name}
+        placeholder='Enter your name'
+        onChange={onChange}
+        />
+      </div>
+      <div classname='form-group'>
+        <input 
+        type='email' 
+        className='form-control'
+        id='email'
+        name='email'
+        value={email}
+        placeholder='Enter your email'
+        onChange={onChange}
+        />
+      </div>
+      <div classname='form-group'>
+        <input 
+        type='password' 
+        className='form-control'
+        id='password'
+        name='password'
+        value={password}
+        placeholder='Enter password'
+        onChange={onChange}
+        />
+      </div>
+      <div classname='form-group'>
+        <input 
+        type='password' 
+        className='form-control'
+        id='passowrd2'
+        name='password2'
+        value={password2}
+        placeholder='Confirm Password'
+        onChange={onChange}
+        />
+      </div>
+      <div classname='form-group'>
+        <button type='submit' classname='btn btn-block'>Submit</button>
+      </div>
+    </form>
+    </section>
+  </>
+  
 }
 
-export default register
+export default Register
